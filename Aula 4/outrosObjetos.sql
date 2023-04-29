20f70edfb60186c16cec725791ee3c553cc1933a
Reinaldo G. P. Neto <reinaldo.neto@ufv.br>
2023-04-28 23:21:51 -0300

    Update outrosObjetos.sql

--- Aula 4/outrosObjetos.sql
+++ Aula 4/outrosObjetos.sql
@@ -19,36 +19,36 @@
 -- incremento: 10
 -- não usar ciclo.
 
-CREATE SEQUENCE 'minha_seq'
+CREATE SEQUENCE "seq_10"
 INCREMENT 10 MINVALUE 1 MAXVALUE 100;
 
 -- 3. Depois da sequência criada faça:
 
     -- A. Inicialize a sequência criada.
-    SELECT NEXTVAL ('minha_seq');
+    SELECT NEXTVAL ('seq_10');
 
     -- B. Apresente o próximo valor da sequência ('minha_seq').
-    SELECT NEXTVAL ('minha_seq');
+    SELECT NEXTVAL ('seq_10');
 
     -- C. Mude o valor da sequência ('minha_seq') para 50
-    SELECT SETVAL('minha_seq', 50);
+    SELECT SETVAL('seq_10', 50);
 
     -- D. Apresente os próximos valores da sequência até dar um erro
-    SELECT NEXTVAL ('minha_seq');
+    SELECT NEXTVAL ('seq_10');
 
     -- E. Altere a sequência para que ela passe a ser cíclica e reinicie a
     -- sequência com valor 1.
-    ALTER SEQUENCE 'minha_seq' RESTART WITH 1 CYCLE;
+    ALTER SEQUENCE "seq_10" RESTART WITH 1 CYCLE;
 
     -- F. Faça testes e verifique se a sequência irá retornar à 1 quando chegar
     -- ao valor 100.
-    SELECT NEXTVAL ('minha_seq');
+    SELECT NEXTVAL ('seq_10');
 
 -- 4. Crie uma tabela chamada dados_multimidia de acordo com as informações
 -- mostradas a seguir:
 
 CREATE TABLE dados_multimidia (
-código SERIAL PRIMARY KEY,
+codigo SERIAL PRIMARY KEY,
 nome VARCHAR(30),
 tipo VARCHAR (20),
 oid_dados OID);
@@ -57,7 +57,7 @@ oid_dados OID);
 -- anteriormente.
 
 INSERT INTO dados_multimidia (nome, tipo, oid_dados) VALUES
-('Familia', 'imagem', lo_import('C:\teste.jpg'));
+('Castle', 'imagem', lo_import('C:\Users\Public\Pictures\hogwarts-legacy.jpg'));
 
 -- 6. Visualize os dados na tabela dados_multimidia e na tabela pg_largeobject
 -- para verificar se a imagem foi importada corretamente para o banco de dados.
@@ -68,6 +68,6 @@ SELECT * FROM pg_largeobject;
 -- 7. Recupere a imagem armazenada no banco de dados com o nome
 -- nova_imagem.jpg. Verifique se a imagem foi exportada corretamente.
 
-SELECT lo_export(oid_dados, 'D:/export_imagem.jpg')
+SELECT lo_export(oid_dados, 'D:/nova_imagem.jpg')
 FROM dados_multimidia
-WHERE nome = 'Familia';
+WHERE nome = 'Castle';
